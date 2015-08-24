7fcc435b907d6ac86da16d730f74691158cce47d
Charles Determan <cdetermanjr@gmail.com>
2015-08-24 11:17:28 -0500

    Update detectCPUs.cpp

--- detectCPUs.cpp
+++ detectCPUs.cpp
@@ -1,6 +1,5 @@
 
 #define __CL_ENABLE_EXCEPTIONS
-#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
 
 #if defined(__APPLE__) || defined(__MACOSX)
 #include <OpenCL/cl.h>
@@ -12,9 +11,6 @@
 #undef CL_VERSION_1_3
 #endif
 
-#ifdef CL_VERSION_1_2
-#undef CL_VERSION_1_2
-#endif
 
 #if defined(__APPLE__) || defined(__MACOSX)
 #include <OpenCL/cl.hpp>
